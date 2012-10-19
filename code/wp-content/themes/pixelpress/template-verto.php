69e7862c2b379c9348ad2bd7ff81f0052378cb5a
Jeffrey Cheung <jeffrey_cheung8@hotmail.com>
2012-10-19 08:35:39 -0400

    Verto

--- code/wp-content/themes/pixelpress/template-verto.php
+++ code/wp-content/themes/pixelpress/template-verto.php
@@ -14,37 +14,59 @@
     	
 		<section id="main" class="fullwidth">
 
-        <table class="verto" width="100%">
-        	<tr><td colspan="2" height="250" background="<?php bloginfo('template_directory');?>/images/verto-banner-1024x250.png"><div id="verto-title">Technology Empowering People</div></td></tr>
+        <table class="verto"><tr><td colspan="2" height="250" background="<?php bloginfo('template_directory');?>/images/verto-banner-1024x250.png"><div id="verto-title">Technology Empowering People</div></td></tr></table>
 
-        	<tr><td colspan="2"><div id="verto-summary">
-	        	<span id="name">Verto,</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et sodales eros. Nunc aliquet, nunc quis adipiscing tempor, justo justo rhoncus dolor, vel tempor quam velit vel nisl. Donec pulvinar, orci quis eleifend hendrerit, sem eros adipiscing mauris, eget egestas dui arcu dapibus felis. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur odio nisl, bibendum in volutpat non, pellentesque venenatis libero. Nam fringilla congue libero, eu pellentesque ligula pretium sed. Aenean ultricies, tortor ac molestie laoreet, tortor leo dapibus sapien, id iaculis turpis mi sed velit. Duis ligula tortor, laoreet ac ultricies ut, convallis sed dui.<br /><br />
+        <div class="verto-summary">
+	        <span id="name">Verto,</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et sodales eros. Nunc aliquet, nunc quis adipiscing tempor, justo justo rhoncus dolor, vel tempor quam velit vel nisl. Donec pulvinar, orci quis eleifend hendrerit, sem eros adipiscing mauris, eget egestas dui arcu dapibus felis. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur odio nisl, bibendum in volutpat non, pellentesque venenatis libero. Nam fringilla congue libero, eu pellentesque ligula pretium sed. Aenean ultricies, tortor ac molestie laoreet, tortor leo dapibus sapien, id iaculis turpis mi sed velit. Duis ligula tortor, laoreet ac ultricies ut, convallis sed dui.<br /><br />
 
 Pellentesque in feugiat elit. Vivamus ligula purus, consectetur eget ultrices id, commodo eget enim. Donec nec augue et ipsum interdum viverra. Ut at tortor non augue convallis tempor. Suspendisse potenti. Donec vitae viverra justo. Nam vel arcu eget dolor pharetra commodo et sed felis. Quisque urna magna, vehicula quis sodales et, aliquet eu arcu. Sed eleifend neque quis tellus facilisis interdum. Cras eget urna ut purus cursus accumsan sit amet sed augue. Etiam in mattis ante.<br /><br />
 
 Duis eu elit lectus. Donec aliquam, sapien id posuere pellentesque, metus turpis auctor eros, feugiat eleifend nisl purus eu elit. Sed porttitor magna facilisis turpis fermentum eget tincidunt enim sollicitudin. Nunc at mauris non lacus dictum condimentum. Aliquam rutrum dui pharetra neque molestie tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin iaculis risus sit amet dolor malesuada fermentum. Proin id libero mi. Cras tortor purus, vehicula eleifend lacinia sed, iaculis non quam. Vestibulum eget eros neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam euismod dignissim volutpat. In at mauris libero, non ultricies est.</span>
-	        	</div></td></tr>
+	   </div>
 
-        	<tr><td colspan="2"><span id="black-text">Our </span><span id="blue-text">Team</span></td></tr>
+        <div class="verto-our-team">
+            <div><span id="black-text">Our </span><span id="blue-text">Team</span></div><br />
+            <div id="team">
+                <span id="left-arrow"><img src="<?php bloginfo('template_directory');?>/images/ico-arrow-left.png" /></span>
+                <span id="pictures">[Picture 1] [Picture 2] [Picture 3] [Picture 4] [Picture 5]</span>
+                <span id="right-arrow"><img src="<?php bloginfo('template_directory');?>/images/ico-arrow-right.png" /></span>
+            </div>
+            <div id="name">Verto Support</div> 
+            <div id="position">Tacos</div>
+        </div>
 
-        	<tr><td colspan="2"><div id="name">Verto Support</div> <div id="position">Tacos</div></td></tr>
+        <div class="separator-dark"></div>
 
-        	<tr><td colspan="2"><div id="separator-dark"></div></td></tr>
+        <div class="verto-our-vision">
+        	<div><span id="black-text">Our </span><span id="blue-text">Vision</span></div><br />
+        	<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+            <div id="quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit."</div>
+            <div>Proin volutpat nisl vel enim congue id fringilla est vestibulum. Donec pretium mollis hendrerit. Nullam nec felis eget velit laoreet sollicitudin luctus ut enim. Cras elit nisi, posuere vitae tincidunt id, posuere ac dolor.</div>
+            <div><ul>
+                <li><b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit.</li>
+                <li><b>Proin volutpat nisl vel enim congue id fringilla est vestibulum.</b> Donec pretium mollis hendrerit. Nullam nec felis eget velit laoreet sollicitudin luctus ut enim.</li>
+                <li><b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit.</li>
+            </ul></div>
+        </div>
 
-        	<tr>
-        		<td class="left" width="45%">
-        			<div><span id="black-text">Our </span><span id="blue-text">Vision</span></div><br />
-        			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat nisl vel enim congue id fringilla est vestibulum. Donec pretium mollis hendrerit. Nullam nec felis eget velit laoreet sollicitudin luctus ut enim. Cras elit nisi, posuere vitae tincidunt id, posuere ac dolor. Cras commodo viverra congue. Fusce elit mi, scelerisque tincidunt elementum sed, fringilla a lectus. Cras bibendum, magna et tristique tempus, nibh nulla tempor lectus, et mattis lacus massa sit amet mi. Nunc vel sapien mi, in scelerisque tellus. Nullam consectetur nibh nec nunc placerat quis egestas enim auctor. Quisque lacinia odio ac eros condimentum fringilla. Cras egestas auctor sodales. Morbi semper nisl nec diam mollis dignissim. Etiam neque massa, posuere quis aliquam eu, molestie non lorem. Cras interdum vulputate neque vel vulputate. Sed cursus consectetur tellus sed aliquam. Donec vestibulum luctus erat, nec facilisis erat auctor iaculis.
-        		</td>
+        <div class="verto-our-benefits">
+            <div><span id="black-text">Our </span><span id="blue-text">Benefits</span></div><br />
+        	<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat nisl vel enim congue id fringilla est vestibulum. Donec pretium mollis hendrerit. Nullam nec felis eget velit laoreet sollicitudin luctus ut enim. Cras elit nisi, posuere vitae tincidunt id, posuere ac dolor.</div>
+            <div><ul>
+                <li>Cras commodo viverra congue.</li>
+                <li>Fusce elit mi, scelerisque tincidunt elementum sed, fringilla a lectus.</li>
+                <li>Cras bibendum, magna et tristique tempus, nibh nulla tempor lectus, et mattis lacus massa sit amet mi.</li>
+                <li>Nunc vel sapien mi, in scelerisque tellus.</li>
+                <li>Nullam consectetur nibh nec nunc placerat quis egestas enim auctor.</li>
+            </ul></div>
+        </div>
 
-        		<td class="right" width="45%">
-        			<div><span id="black-text">Our </span><span id="blue-text">Benefits</span></div><br />
-        			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat nisl vel enim congue id fringilla est vestibulum. Donec pretium mollis hendrerit. Nullam nec felis eget velit laoreet sollicitudin luctus ut enim. Cras elit nisi, posuere vitae tincidunt id, posuere ac dolor. Cras commodo viverra congue. Fusce elit mi, scelerisque tincidunt elementum sed, fringilla a lectus. Cras bibendum, magna et tristique tempus, nibh nulla tempor lectus, et mattis lacus massa sit amet mi. Nunc vel sapien mi, in scelerisque tellus. Nullam consectetur nibh nec nunc placerat quis egestas enim auctor. Quisque lacinia odio ac eros condimentum fringilla. Cras egestas auctor sodales. Morbi semper nisl nec diam mollis dignissim. Etiam neque massa, posuere quis aliquam eu, molestie non lorem. Cras interdum vulputate neque vel vulputate. Sed cursus consectetur tellus sed aliquam. Donec vestibulum luctus erat, nec facilisis erat auctor iaculis.
-        		</td>
-        	</tr>
+        <!-- START Temporary Fix -->
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <!-- END Temporary Fix -->
 
-        	<tr><td colspan="2"><div id="separator-dark"></div></td></tr>
-        </table>
+        <div class="separator-dark"></div>
+
 		</section><!-- /#main -->
 		
 		<?php woo_main_after(); ?>
